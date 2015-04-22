624262c492833c50824f5be3d049c6a73efbd295
Vinnie Magro <vmagro@socaldevs.com>
2015-04-22 15:36:18 -0700

    Copy new VGA logic to bullet

--- Bullet.v
+++ Bullet.v
@@ -20,22 +20,26 @@
 //
 //////////////////////////////////////////////////////////////////////////////////
 module Bullet(
-	 input Clk,
-	 input Reset,
-    input Bullet_Fired,
-    input [8:0] Aliens_Row,
-    input [9:0] Aliens_Col,
-	 input [8:0] Player_Row,
-    input [9:0] Player_Col,
-    output reg [8:0] Bullet_Row,
-    output reg [9:0] Bullet_Col,
-    output Aliens_Defeated,
-    output Bullet_Onscreen,
-	 output reg [49:0] Aliens_Grid
-    );
-	
+	input Clk,
+	input Reset,
+	input Bullet_Fired,
+	input [8:0] Aliens_Row,
+	input [9:0] Aliens_Col,
+	input [8:0] Player_Row,
+	input [9:0] Player_Col,
+	output reg [8:0] Bullet_Row,
+	output reg [9:0] Bullet_Col,
+	output Aliens_Defeated,
+	output Bullet_Onscreen,
+	output reg [49:0] Aliens_Grid
+);
+
 	//assign Aliens_Grid = 50'h3FFFFFFFFFFFF;
-	
+	reg[3:0] AlienH;
+	reg[3:0] AlienW;
+	reg[9:0] x_t;
+	reg[9:0] y_t;
+
 	parameter AlienWidth = 30;
 	parameter PlayerWidth = 30;
 	parameter AlienWidthSpacing = 10;
@@ -43,17 +47,17 @@ module Bullet(
 	parameter PlayerHeight = 20;
 	parameter AlienHeightSpacing = 10;
 	parameter NumCols = 10;
-	
+
 	integer i, j;
-	
+
 	assign Bullet_Onscreen = ((Bullet_Row > 0) && (Bullet_Row < 480));
 	assign Aliens_Defeated = (Aliens_Grid == 0);
-	
+
 	always @(posedge Clk) begin
 		if (Reset) begin
-			Aliens_Grid <= 50'h3FFFFFFFFFFFF;
-			Bullet_Row <= 500;
-			Bullet_Col <= 9'bXXXXXXXXX;
+		  Aliens_Grid <= 50'h3FFFFFFFFFFFF;
+		  Bullet_Row <= 500;
+		  Bullet_Col <= 9'bXXXXXXXXX;
 		end
 		else begin
 			if (Bullet_Fired && ~Bullet_Onscreen) begin
@@ -63,17 +67,16 @@ module Bullet(
 			if (Bullet_Onscreen) begin
 				Bullet_Row <= Bullet_Row - 10;
 			end
-			for (i=0; i < 5; i = i+1) begin
-				for (j=0; j < 10; j = j+1) begin
-					//is it in the right row to hit an alien?
-					if ((Bullet_Row >= (j * (AlienWidth + AlienWidthSpacing))) && (Bullet_Row <= (j * (AlienWidth + AlienWidthSpacing) + AlienWidth))) begin
-						if ((Bullet_Col >= (i * (AlienHeight + AlienHeightSpacing))) && (Bullet_Col <= (i * (AlienHeight + AlienHeightSpacing)))) begin
-							if (Aliens_Grid[i * NumCols + j] == 1) begin
-								Aliens_Grid[i * NumCols + j] <= 0;
-								Bullet_Row <= 500;
-							end
-						end
-					end
+			if (Bullet_Col >= Aliens_Col && Bullet_Row >= Aliens_Row) begin
+				x_t = Bullet_Col - Aliens_Col;
+				y_t = Bullet_Row - Aliens_Row;
+				x_t = x_t % (AlienWidth + AlienWidthSpacing);
+				AlienH = x_t / (AlienWidth + AlienWidthSpacing);
+				y_t = y_t % (AlienHeight + AlienHeightSpacing);
+				AlienW = y_t / (AlienHeight + AlienHeightSpacing);
+				if (x_t < AlienWidth && y_t < AlienHeight && Aliens_Grid[AlienW * NumCols + AlienH]) begin
+					Aliens_Grid[AlienW * NumCols + AlienH] <= 0;
+					Bullet_Row <= 500;
 				end
 			end
 		end
